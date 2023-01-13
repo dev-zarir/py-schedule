a4ac2a6fb48148c53fb82535285c2ce199d167ba
S M Shahriar Zarir <98312938+dev-zarir@users.noreply.github.com>
2023-01-13 18:23:31 +0600

    Update helper.py

--- helper.py
+++ helper.py
@@ -6,7 +6,7 @@ from time import sleep
 # Required Info
 target_id='100063442311391'
 wish_msg="Happy birthday, Nazia! I hope this special day is as amazing as you are. You deserve all the love, laughter, and birthday cheer that comes your way. I'm so grateful to have you as a friend and I can't wait to see all the amazing things you'll accomplish in the year ahead. Here's to another year of friendship, growth, and endless memories. May your birthday be as special as you are!"
-cookie='wd=1366x615; fr=0LWH2JUC0zv8yUWoj.AWWLhiwgIoc9BUvPQxQbx7K6XfU.Bjt9kl.xf.AAA.0.0.Bjt938.AWUfygvdLMA; sb=Jdm3Y6wXlI8cPMcdpWhvSGXm; datr=Jdm3YzGKkDqXX8uzEjo7SoMr; c_user=100075924800901; xs=39%3ADdy1ToEAVlJkEQ%3A2%3A1672993075%3A-1%3A4472; usida=eyJ2ZXIiOjEsImlkIjoiQXJvMjIzNjFlZnJza3IiLCJ0aW1lIjoxNjcyOTk2NzY0fQ%3D%3D; m_page_voice=100075924800901'
+cookie='datr=XEuyY97oRNVS4sVsqW5CFL2z; sb=XEuyY6B-jjrLlPIfblDVfxOB; m_pixel_ratio=1.84375; vpd=v1%3B752x391x1.84375; usida=eyJ2ZXIiOjEsImlkIjoiQXJudTlmMGpsMmJsMiIsInRpbWUiOjE2NzI2Mjk5NDN9; fr=0sua9pMLyO9nO6huE.AWX-2T4VtiI3UE2BwgoLA0X7bJ4.Bjsktc.yP.AAA.0.0.BjwUyQ.AWU3Nbu4qY4; c_user=100075924800901; xs=34%3ASP--0b33retfsg%3A2%3A1673612433%3A-1%3A4472; m_page_voice=100075924800901; wd=391x752; locale=en_GB; fbl_st=101526618%3BT%3A27893540; fbl_cs=AhC8FccwkuKSmD6k4l3omzFUGGFIbVF6Y2g0bU49QT1ZNmFyZTVjPWozbQ; fbl_ci=538158021535757'
 
 # Sending Function
 def Send_Message(target_id, msg, cookie):
